5aec5fab9e7acff2f30c187a62c79a97ee98c052
Judith Sanchez <judithv.sanchezc@gmail.com>
2022-12-17 12:47:44 -0500

    Added my project

---
+++ Creator.js
@@ -0,0 +1,25 @@
+class Creator {
+   constructor(name,country,facebook,instagram,patreon,twitter,youtube,website) {
+      this.name = name,
+      this.spanish_accent = country,
+      this.socialmedia = {
+         facebook,
+         instagram,
+         patreon,
+         twitter,
+         youtube,
+         website
+      }
+   }
+}
+
+const Creator_Curiosamente = new Creator (
+   "Curiosamente",
+   "México",
+   "https://www.facebook.com/CuriosaMenteTV/",
+   "https://www.instagram.com/curiosamente/?hl=en",
+   "https://www.patreon.com/curiosamente",
+   "https://twitter.com/curiosamente",
+   "https://www.youtube.com/@curiosamente",
+   "https://www.curiosamente.com/"
+)
---
+++ TextWithTranslations.js
@@ -0,0 +1,21 @@
+class TextWithTranslations {
+   constructor(content) {
+      this.content = content;
+
+      this.sections = content.text.sections;
+      this.number_of_sections = content.number_of_sections
+
+      this.number_of_paragraphs = content.number_of_paragraphs
+
+      this.cognates = content.cognates;
+
+      this.challenging_words = content.challenging_words;
+   }
+
+   separate_sentence_into_words(sentence){
+      return sentence.split(/([^\wáéíóúñü])/i);
+   }
+}
+
+const Curiosamente_Video_Huevo_Gallina = new TextWithTranslations (Video_Huevo_Gallina)
+
